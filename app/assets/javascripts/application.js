1aec28118aa5bc3b8da779995c2a91d6f0de39f7
Noah Finnerman <noahfinnerman@gmail.com>
2018-03-26 15:34:08 -0400

    put bootrap js in vendorapps file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,12 +21,4 @@
 //= require_tree .
 
 
-// $(function() {
-//   $('#term').autocomplete({
-//     source: '/contacts/autocomplete',
-//     minLength: 3,
-//     select: function (event, ui) {
-//       $('#term.val(ui,item.value)');
-//     }
-//   });
-// });
+
