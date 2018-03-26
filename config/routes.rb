56d57d1f425fe66c5483b49f3194d8ad7753982a
Noah Finnerman <noahfinnerman@gmail.com>
2018-03-26 00:39:12 -0400

    Add resources for routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
     authenticated do
-    get 'contacts/index', to: 'contacts#index', as: 'contacts'
-    get 'contacts/new', as: 'new_contact'
-    get 'contact/:id/edit', to: 'contacts#edit', as: 'edit_contact'
-    post 'contacts/create', to: 'contacts#create', as: 'create_contact'
-    patch 'contacts/:id/update', to: 'contacts#update', as: 'update_contact'
-    delete 'contacts/:id/destory', to: 'contacts#destroy', as: 'destroy_contact'
+      resources :contacts, except: [:show]
 
-    root 'contacts#index'
+      root 'contacts#index'
   end
 end
