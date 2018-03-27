ba264a36e49bd4b0f76077d1c832cec146bf6fc7
Noah Finnerman <noahfinnerman@gmail.com>
2018-03-27 17:55:02 -0400

    Add material dashbord pro template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,13 +11,60 @@
 // about supported directives.
 //= require vendorapps
 //= require jasny-bootstrap.min
-//= require jquery-ui/core
-//= require jquery-ui/widget
-//= require jquery-ui/position
-//= require jquery-ui/widgets/autocomplete
-//= require jquery-ui/widgets/menu
+//= require jquery-ui/
+
 //= require rails-ujs
+//= require contacts
 //= require turbolinks
 //= require_tree .
+
+
+// safsa//= require jquery-ui/widget
+// cs= require jquery-ui/position
+// cc= require jquery-ui/widgets/autocomplete
+// cc= require jquery-ui/widgets/menu
+//csc= require jquery-ui/core
+
+
+$(function() {
+
+
+   $("#save-new-group").on( "click", function(event) {
+        var newGroup = $('#new_group');
+        // var inputGroup = $('#new_group')
+
+      event.preventDefault();
+      $.ajax({
+        url: "/groups",
+        method: "post",
+        data: {
+          group: {name: $("#new_group").val()}
+        },
+        success: function (group) {
+          console.log(group);
+          if(group.id != null){
+           newGroup.next('.text-danger').detach();
+            $('#new_group').addClass('has-success')
+            .after('<p class="text-success pt-2 mb-0">The Group: ' + '"' + group.name+'"' + ' was successfuly created you can now select it</p>' );
+
+            $('#contact_group_id').append(newOption);
+          }
+        },
+        error: function(err){
+
+          var errors = err.responseJSON;
+          var error = errors.join(", ");
+
+          if (error) {
+           newGroup.next('.text-danger').detach();
+            $('#new_group').addClass('has-error')
+            .after('<p class="text-danger pt-2 mb-0">' + error + '</p>' );
+            }
+        }
+      })
+
+    });
+});
+
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
           get 'autocomplete', on: :collection
 
       end
+
+      post '/groups', to: 'groups#create'
       root 'contacts#index'
   end
 end
