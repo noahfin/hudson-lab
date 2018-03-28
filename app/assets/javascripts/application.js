6e4aadf5efce6f627112c67050da91ab027cc686
Noah Finnerman <noahfinnerman@gmail.com>
2018-03-28 15:48:34 -0400

    Add template form into partial

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,50 +26,6 @@
 //csc= require jquery-ui/core
 
 
-$(function() {
-
-
-   $("#save-new-group").on( "click", function(event) {
-        var newGroup = $('#new_group');
-        // var inputGroup = $('#new_group')
-
-      event.preventDefault();
-      $.ajax({
-        url: "/groups",
-        method: "post",
-        data: {
-          group: {name: $("#new_group").val()}
-        },
-        success: function (group) {
-          console.log(group);
-          if(group.id != null){
-           newGroup.next('.text-danger').detach();
-            $('#new_group').addClass('has-success')
-
-
-            .after('<p class="text-success pt-2 mb-0">The Group: ' + '"' + group.name+'"' + ' was successfuly created you can now select it</p>' );
-            $.notify({
-              title: "New Group Added:",
-              message: '<p class="text-success pt-2 mb-0">The Group: ' + '"' + group.name+'"' + ' was successfuly created you can now select it</p>'
-            });
-            $('#contact_group_id').append(newOption);
-          }
-        },
-        error: function(err){
-
-          var errors = err.responseJSON;
-          var error = errors.join(", ");
-
-          if (error) {
-           newGroup.next('.text-danger').detach();
-            $('#new_group').addClass('has-error')
-            .after('<p class="text-danger pt-2 mb-0">' + error + '</p>' );
-            }
-        }
-      })
-
-    });
-});
 
 
 
--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,6 @@ class Group < ApplicationRecord
 
    validates :name, presence: true
    validates :name, uniqueness: true
+
+
 end
