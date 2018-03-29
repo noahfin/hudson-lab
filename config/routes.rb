d93a26a424dbeb50a89eac04d2d36b39246a013c
Noah Finnerman <noahfinnerman@gmail.com>
2018-03-29 17:09:06 -0400

    Add first and last name to users"

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 
   root to: 'visitors#index'
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users
     authenticated do
       resources :contacts, except: [:show] do
