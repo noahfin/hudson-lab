4711bb3ca3e613b44b5bb5ac7dc2ec720969dedc
Noah Finnerman <noahfinnerman@gmail.com>
2018-04-06 13:29:40 -0400

    Add icon to groups nav

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
    has_many :contacts
-  has_and_belongs_to_many :users
+   has_and_belongs_to_many :users
 
    validates :name, presence: true
    validates :name, uniqueness: true
