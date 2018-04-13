16092fb857cb191397acb89ae79fc66cd8413726
Noah Finnerman <noahfinnerman@gmail.com>
2018-04-13 12:11:44 -0400

    Add backgroud collor to card-6

---
+++ db/migrate/20180413012444_add_fields_to_properties.rb
@@ -0,0 +1,22 @@
+class AddFieldsToProperties < ActiveRecord::Migration[5.1]
+  def change
+    add_column :properties, :rental_rate, :string
+    add_column :properties, :price, :string
+    add_column :properties, :year_built, :string
+    add_column :properties, :building_class, :string
+    add_column :properties, :lease_type, :string
+    add_column :properties, :overhead_doors, :string
+    add_column :properties, :loading_docks, :string
+    add_column :properties, :min_divisible, :string
+    add_column :properties, :space_use, :string
+    add_column :properties, :space_available, :string
+    add_column :properties, :date_available, :string
+    add_column :properties, :property_type, :string
+    add_column :properties, :address, :string
+    add_column :properties, :description, :text
+    add_column :properties, :for_lease, :boolean
+    add_column :properties, :for_sale, :boolean
+    add_column :properties, :owner_name, :string
+    add_reference :properties, :contact, foreign_key: true
+  end
+end
