de9fad5c11419743df927151bff9fa84a998fd1f
Noah Finnerman <noahfinnerman@gmail.com>
2018-04-16 13:31:39 -0400

    Chnage grvatar image size

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   include ActionView::Context
   def gravatar_url(email)
     gravatar = Digest::MD5::hexdigest(email).downcase
-    url = "https://www.gravatar.com/avatar/#{gravatar}?s=200"
+    url = "https://www.gravatar.com/avatar/#{gravatar}"
   end
   def flash_class(level)
     case level
