b3b6a38f67ae827e0eeee560886a426eba8ae2f0
Noah Finnerman <noahfinnerman@gmail.com>
2018-05-14 09:55:10 -0400

    Fix deals controller

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -14,17 +14,17 @@ class DealsController < ApplicationController
  def category
      @deals = Deal.all
      @categories = Deal.pluck(:category).uniq
-      @similar_properties = []
+      @similar_deals = []
        @categories.each do |cat|
 
-   @similar_properties << Deal.where("category like ?", "%#{cat}")
+   @similar_deals << Deal.where("category like ?", "%#{cat}")
    end
  end
    def show
-    @similar_properties = Deal.where("category like ?", "%#{@property.category}")
+
    end
    def create
-      @deal = Deal.new(property_params)
+      @deal = Deal.new(deal_params)
       if @deal.save
 
         params['contact_ids'].each_with_index do |c_id, i|
@@ -45,7 +45,7 @@ class DealsController < ApplicationController
  def update
 @deal = Deal.find(params[:id])
 
-    @deal.update_attributes(property_params)
+    @deal.update_attributes(deal_params)
        flash[:info] = "Deal Was Updated."
         params['contact_ids'].each_with_index do |c_id, i|
           next if c_id.to_i == 0
@@ -60,7 +60,7 @@ class DealsController < ApplicationController
    private
 
     def deal_params
-      params.require(:property).permit(:name, :code, :category :contact_ids)
+      params.require(:property).permit(:name, :code, :category, :contact_ids)
     end
     def find_property
       @deal = Deal.find params[:id]
