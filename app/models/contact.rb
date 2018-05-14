3cae8b179ba2039a0c4106e11a57d292ca7f4253
Noah Finnerman <noahfinnerman@gmail.com>
2018-05-14 14:36:47 -0400

    Fix touches contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,8 +5,8 @@ class Contact < ApplicationRecord
   has_and_belongs_to_many :user
   has_and_belongs_to_many :properties
   has_and_belongs_to_many :tasks
-  has_many :touches, class_name: 'Touch'
-  has_many :leads, class_name: 'Lead'
+  has_many :touches
+  has_many :leads
   has_many :current_users, -> { users }, class_name: 'User'
   has_attached_file :avatar, styles: { medium: "150x150>", thumb: "100x100>" },
   :s3_credentials => "#{Rails.root}/config/s3.yml",
