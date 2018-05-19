1020068d1ff57b596952b59caf45325c22e8b2cc
Noah Finnerman <noahfinnerman@gmail.com>
2018-05-19 13:44:44 -0400

    Add missing fields to prams

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -90,7 +90,7 @@ class ContactsController < ApplicationController
   private
 
   def contact_params
-    params.require(:contact).permit(:name,  :email, :company, :address, :phone, :cell, :county, :state,:country, :postal_code, :notes, :city, :street_num, :strret_name, :group_id, :contact_id, :role, :verified, :avatar,{:user_id => []}, {:group_id => []})
+    params.require(:contact).permit(:name,  :email, :company, :address, :phone, :cell, :county, :state,:country, :postal_code, :notes, :city, :street_num, :strret_name, :prefix, :first_name, :middle_name, :last_name, :suffix, :owns_cents, :year_of_Founding, :primary_industry, :web_address, :latitude, :longitude, :type, :facility_size, :total_number_of_employees, :postion, :sic, :group_id, :contact_id, :role, :verified, :avatar,{:user_id => []}, {:group_id => []})
   end
 
   def find_contact
