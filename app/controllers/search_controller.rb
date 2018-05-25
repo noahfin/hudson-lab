0cdde82bfa13839fcf6a1eb99075dc9b32fdc54d
Noah Finnerman <noahfinnerman@gmail.com>
2018-05-25 11:35:36 -0400

    Add index html to js file load

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
 def create
      if params[:query].present?
-       @contacts = Contact.search(params[:query]).order('name ASC').page(params[:page])
+       @contacts = Contact.search(params[:query]).order('first_name ASC').page(params[:page])
 
       respond_to do |format|
         format.html
