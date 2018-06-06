e0deb6dcc2fbe4fc73888fd0376589aa976ff123
Noah Finnerman <noahfinnerman@gmail.com>
2018-06-06 14:04:45 -0400

    Add paperclip avatar to projects

---
+++ app/views/steps/_step.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! step, :id, :name, :description, :start_time, :finsh_time, :started_time, :finshed_time, :created_at, :updated_at
+json.url step_url(step, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180606140404) do
+ActiveRecord::Schema.define(version: 20180606172819) do
 
   create_table "account_logins", force: :cascade do |t|
     t.string "ipAddress"
@@ -267,6 +267,10 @@ ActiveRecord::Schema.define(version: 20180606140404) do
     t.boolean "technology"
     t.boolean "business"
     t.decimal "budget"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "projects_steps", id: false, force: :cascade do |t|
