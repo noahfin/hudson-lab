0d6c8f668886a1ad8eb2ae0ae0d5fa6f8f19347c
Noah Finnerman <noahfinnerman@gmail.com>
2018-10-25 15:29:22 -0400

    Add fix to appontments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -9,16 +9,9 @@ class Appointment < ApplicationRecord
 
   # Notify our appointment attendee X minutes before the appointment time
   def reminder
-    @twilio_number = ENV['TWILIO_NUMBER']
-    account_sid = ENV['TWILIO_ACCOUNT_SID']
-    @client = Twilio::REST::Client.new account_sid, ENV['TWILIO_AUTH_TOKEN']
-    time_str = ((self.time).localtime).strftime("%I:%M%p on %b. %d, %Y")
-    reminder = "Hi #{self.name}. Just a reminder that you have an appointment coming up at #{time_str}."
-    message = @client.api.account(account_sid).messages.create(
-      :from => @twilio_number,
-      :to => self.phone_number,
-      :body => reminder,
-    )
+       time_str = ((self.time).localtime).strftime("%I:%M%p on %b. %d, %Y")
+       message = "Your appontment '#{@appointment.name}' is at '#{time_str}'"
+      TwilioTextMessenger.new(message).call(self.phone_number)
   end
 
   def when_to_run
