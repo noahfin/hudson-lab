039e23fac860f12035d59ce24bb407a75d09413a
Noah Finnerman <noahfinnerman@gmail.com>
2018-11-13 16:04:13 -0500

    Add image to contacts

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,19 +2,17 @@ class Company < ApplicationRecord
    has_and_belongs_to_many :address
    has_and_belongs_to_many :contacts
    has_and_belongs_to_many :deals
-    has_attached_file :avatar,
+   has_attached_file :avatar,
   :storage => :s3,
   :styles => {
-  thumb: '100x100#',
-  square: '200x200>',
-  medium: '540x283'
-},
+    thumb: '100x100#',
+    square: '200x200>',
+    medium: '540x283'
+  },
   default_url: "ant-rozetsk.jpg",
   :s3_credentials => "#{Rails.root}/config/s3.yml",
   :bucket => 'hudson-lab-images',
   :url => ":s3_domain_url",
   :path => "/:class/avatars/:id_:basename.:style.:extension"
-
-
-validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
