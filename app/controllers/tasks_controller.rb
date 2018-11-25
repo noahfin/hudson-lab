46a27fbea24ba2e34d2021f3dbff8ad14b8b4350
Noah Finnerman <noahfinnerman@gmail.com>
2018-11-25 13:48:20 -0500

    Fix is personL

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,6 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
-
   def index
     @tasks = Task.order(created_at: :desc)
     respond_to do |format|
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,43 +1,33 @@
 module TasksHelper
   def task_relationships(contacts, groups, projects, properties, leads)
-     @tasks = Task.all.order(:name)
-        if contacts
-          contacts = contacts
-          contact_array = []
-          contacts.each do |contact_id|
-          next if contact_id == ""
-             if contact_id.to_i > 0
-              contact_array.push(contact_id)
-                 @task.contact_ids = contact_array
-              end
-           end
-         end
-
-         if groups
-          groups.each do |group_id|
-            next if group_id == 0 || group_id == ''
-            group = Group.find(group_id)
-              @task.groups << group
+    if contacts
+      contacts = contacts
+      contact_array = []
+      contacts.each do |contact_id|
+      next if contact_id == ""
+         if contact_id.to_i > 0
+          contact_array.push(contact_id)
+             @task.contact_ids = contact_array
           end
-         end
-         if params['task']['contact_ids']
-         contacts = params['task']['contact_ids']
-         contact_array = []
-         contacts.each do |contact_id|
-         next if contact_id == ""
-          contact = Contact.find(contact_id)
+       end
+     end
 
-             @task.contact_ids << contact
-           end
-         end
-         if projects
-             @task.project_ids = projects
-         end
-         if leads
-             @task.lead_ids = leads
-         end
-           if properties
-             @task.property_ids = properties
-         end
+     if groups
+      groups.each do |group_id|
+        next if group_id == 0 || group_id == ''
+        group = Group.find(group_id)
+          @task.groups << group
+      end
+     end
+
+     if projects
+         @task.project_ids = projects
+     end
+     if leads
+         @task.lead_ids = leads
+     end
+       if properties
+         @task.property_ids = properties
+     end
   end
 end
